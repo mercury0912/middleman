e386d7483f77a531211023e3bcf25affeedd82da
mercury0912 <24903720@qq.com>
2018-08-07 21:47:45 +0800

    import: delete unused import

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
 from setuptools import setup, find_packages
 
 with open("README.rst") as f:
