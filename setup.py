f8744162b987884f2d5dce136baf992696cbac3a
mercury0912 <24903720@qq.com>
2018-08-07 20:23:22 +0800

    setuptools: Simplify metadata assignment

--- setup.py
+++ setup.py
@@ -1,38 +1,49 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2018 mercury
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import os
-import re
-
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-readme = open(os.path.join(here, 'README.rst')).read()
-
-about = {}
-path_to_about = os.path.join(here, "middleman", "__about__.py")
-with open(path_to_about) as file:
-    # Assign a dictionary representing the __about__'s global symbol table to 
-    # about. After that about looks like {'title' : 'middleman', ...}
-    exec(file.read(), about) 
-    
+with open("README.rst") as f:
+    long_description = f.read()
 
 setup(
-    name=about["__title__"],
-    version=about["__version__"],
+    name="mmiddleman",
+    version="0.0.1",
     packages=find_packages(),
-    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    zip_safe=False,
     include_package_data=True,
-    
-    # metadata to display on PyPI
-    description=about["__summary__"],
-    long_description=readme,
+    description=("middleman is a package which provides a chance to know more"
+                 " about the world to you."),
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author="mercury",
+    author_email="24903720@qq.com",
+    url="https://github.com/mercury0912/middleman",
+    license="MIT",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: End Users/Desktop",
         "Operating System :: POSIX :: Linux",
-        "Topic: :: Internet :: Proxy Servers",
+        "Topic :: Internet :: Proxy Servers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -42,14 +53,9 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    license=about["__license__"],
-    url=about["__url__"],
-    author=about["__author__"],
-    author_email=about["__email__"],
-    
+    python_requires=">=3.4",
+    zip_safe=False,
     entry_points={
-        'console_scripts' : [
-        'middleman = middleman.server:main',
-        ]
+        'console_scripts' : ['middleman = middleman.server:main']
     }
 )
