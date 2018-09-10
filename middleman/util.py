800f17150f7b665da7e6828f26a68ffc1d10a821
mercury0912 <24903720@qq.com>
2018-09-10 12:02:15 +0800

    signaleon: add signaleon tool

--- middleman/util.py
+++ middleman/util.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 
 
 def _print_message(message, file=None):
@@ -38,3 +39,26 @@ def fileobj_to_fd(fileobj):
     if fd < 0:
         raise ValueError("Invalid file descriptor: {}".format(fd))
     return fd
+
+
+def singleton(cls):
+    """ Use class as singleton. """
+    # see also https://wiki.python.org/moin/PythonDecoratorLibrary#Singleton
+
+    cls.__new_original__ = cls.__new__
+
+    @functools.wraps(cls.__new__)
+    def singleton_new(cls, *args, **kw):
+        it = cls.__dict__.get('__it__')
+        if it is not None:
+            return it
+
+        cls.__it__ = it = cls.__new_original__(cls, *args, **kw)
+        it.__init_original__(*args, **kw)
+        return it
+
+    cls.__new__ = singleton_new
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
+
+    return cls
